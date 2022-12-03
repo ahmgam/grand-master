8683741e24b7be7562753c374383bd0033c50e9a
ahmgam <50333822+ahmgam@users.noreply.github.com>
2022-12-03 02:20:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 source /usr/share/gazebo/setup.sh
 source /opt/ros/noetic/setup.bash
-cd /gzweb
-roslaunch roslaunch robot_ws/src/multirobot_sim/launch/world-map-docker.launch && npm start 
+source /root/robot_ws/devel/setup.bash
+chmod -R +rx /root/robot_ws/src/multirobot_sim
+roscore & roslaunch --wait /root/robot_ws/src/multirobot_sim/launch/world-map-docker.launch & npm start
